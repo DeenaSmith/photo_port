83b19091e1ce0b5290c9b742c0bb8e230a5a5e6a
Deena Smith <deenasmith1630@gmail.com>
2022-06-17 16:32:44 -0500

    photos added and displayed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@
 import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-import Gallery from './components/Gallery';
-
+import Gallery from './components/Gallery/index';
 
 function App() {
   const [categories] = useState([
@@ -14,10 +13,10 @@ function App() {
     { name: 'portraits', description: 'Portraits of people in my life' },
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-  ]);
+  ]); 
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
+console.log(currentCategory, 'THIS THING');
   return (
     <div>
       <Nav
@@ -26,14 +25,11 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
-          <About></About>
-        </div>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
 }
-
 
 export default App;
