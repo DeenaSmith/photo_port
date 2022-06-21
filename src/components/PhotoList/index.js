94f1f3100009e0421691a547da23b84a74ae6a0c
Deena Smith <deenasmith1630@gmail.com>
2022-06-21 10:52:17 -0500

    selected images now delete on click

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -127,13 +127,15 @@ const PhotoList = ({ category }) => {
 
     const toggleModal = (image, i) => {
         setCurrentPhoto({...image, index: i})
-        setIsModalOpen(true);
+        setIsModalOpen(!isModalOpen);
     }
 
     // src/assets/small/commercial/0.jpg
     return (
         <div>
-            {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+            {isModalOpen && (
+                <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+            )}
             <div className="flex-row">
                 {currentPhotos.map((image, i) => (
                     <img
